49bae810dc1703ceb015cb724e11910c90df43a5
div-ang <angapaydivin@gmail.com>
2021-07-24 22:59:47 +0100

    reset password

--- routes/web.php
+++ routes/web.php
@@ -44,5 +44,5 @@ Route::get('/sous-categorie/supprimer/{id}/{containerId}', [App\Http\Controllers
 Route::get('/administration', [App\Http\Controllers\AdministrationController::class, 'get'])->middleware('roles:admin');
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home')->middleware('roles:admin');
 Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.request');
-Route::post('password/reset', 'Auth\ResetPasswordController@postReset')->name('password.reset');
+Route::post('password/reset', 'Auth\ResetPasswordController@postReset')->name('password.reset.token');
 
