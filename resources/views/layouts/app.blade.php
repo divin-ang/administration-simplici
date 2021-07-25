44336e7b03b530f3786adf2f76e22e65a57aad7b
div-ang <angapaydivin@gmail.com>
2021-07-25 01:21:22 +0100

    'email'

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -52,19 +52,10 @@
                                 </li>
                             @endif
                         @else
-                         <li>
-
-                          <a class="dropdown-item" href="{{ route('utilisateurs') }}">
-                                   Gerer les utilisateurs    
-                             </a>
-
-                            </li>
                             <li class="nav-item dropdown">
-                                 
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }}
                                 </a>
-                           
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                                     <a class="dropdown-item" href="{{ route('logout') }}"
@@ -78,7 +69,6 @@
                                     </form>
                                 </div>
                             </li>
-
                         @endguest
                     </ul>
                 </div>
