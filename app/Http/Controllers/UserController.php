170a4c34aa69908493e7ad30970c17bd344e7cf5
div-ang <angapaydivin@gmail.com>
2021-07-25 03:01:07 +0100

    'email'

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
+
+use Illuminate\Support\Facades\Auth;
+
 class UserController extends Controller
 {
     //
