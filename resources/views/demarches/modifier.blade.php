9aa4cc5a30b678304399d5570bc158024d493162
div-ang <angapaydivin@gmail.com>
2021-07-27 15:43:22 +0100

    ajout d'une demarche

--- resources/views/demarches/modifier.blade.php
+++ resources/views/demarches/modifier.blade.php
@@ -86,40 +86,21 @@
           
           
           </div>
-
-
-
-         
-            
-            
-
-
-            
       
 
-          
-          
+         </div>
          
-
-
-
-
-
-          <div class="form-group row mb-0">
+         
+        
+         
+         </div>
+            <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
                                     {{ __('Enregistrer') }}
                                 </button>
                             </div>
          </div>
-
-         </div>
-         
-         
-        
-         
-         </div>
-        
 
 </form>
                 </div>
