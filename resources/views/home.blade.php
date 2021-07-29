e3bd15936894b37f8051f5087ae88aabab03750b
div-ang <angapaydivin@gmail.com>
2021-07-29 16:13:47 +0100

    users

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,7 @@
     <div class="row justify-content-center">
         <div class="col-md-11">
            <a href="{{ url('/utilisateurs') }}" class="user_link ">Gérer les utilisateurs</a>
-           <a href="{{ url('/services'.'/liste') }}" class="user_link ">Gérer les utilisateurs</a>
+           <a href="{{ url('/services'.'/liste') }}" class="user_link ">Les fourniseurs des démarches</a>
                              <a href="{{ url('/demarche'.'/supprimer') }}" class="user_link">Supprimer une démarche de toutes les sous catégories</a>
             <div class="card">
             
