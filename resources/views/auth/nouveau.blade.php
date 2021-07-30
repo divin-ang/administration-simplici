cc8cc481538481b7aa602f5fbb14bd40d644e082
div-ang <angapaydivin@gmail.com>
2021-07-30 12:40:07 +0100

    users admin

--- resources/views/auth/nouveau.blade.php
+++ resources/views/auth/nouveau.blade.php
@@ -1,3 +1,6 @@
+@extends('layouts.app')
+
+@section('content')
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
@@ -5,7 +8,7 @@
                 <div class="card-header">{{ __('Création de compte') }}</div>
 
                 <div class="card-body">
-                    <form method="POST" action="{{ route('newUser') }}">
+                    <form method="POST" action="{{ route('register') }}">
                         @csrf
 
                         <div class="form-group row">
@@ -36,20 +39,6 @@
                             </div>
                         </div>
 
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Mot de passe') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
 
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
@@ -63,3 +52,5 @@
             </div>
         </div>
     </div>
+</div>
+@endsection
