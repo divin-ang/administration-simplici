0dd22f44435fb351532d8cc881be2be0b419785a
div-ang <angapaydivin@gmail.com>
2021-08-05 00:54:53 +0100

    ajout d'images tutoriel

--- resources/views/demarches/duppliquerDocument.blade.php
+++ resources/views/demarches/duppliquerDocument.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Ajouter une démarche existante dans ') }} : {{$subcontainer_name}}</div>
+                <div class="card-header">{{ __('Ajouter un document existant ') }} : {{}}</div>
 
                 <div class="card-body">
                    
