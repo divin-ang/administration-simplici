e95fc815f5453aeb7b1ec585a8178fde6bf191fd
div-ang <angapaydivin@gmail.com>
2021-08-04 10:54:25 +0100

    ajout d'images tutoriel

--- resources/views/tutoriel/tutoriel.blade.php
+++ resources/views/tutoriel/tutoriel.blade.php
@@ -30,14 +30,17 @@
                            Pour modifier une catégorie : cliquer sur le bouton modifier à droite du nom de la catégorie concernée.
                         
                         </p>
-                        <div class="">
+                        <div class="row">
 
   
                           <img src="{{ asset('image/site-accueil.png') }}" alt="accueil" class="img-fluid accueil">
-                        
+                          <img src="{{ asset('image/cat-modifier.PNG') }}" alt="accueil" class="  img-fluid accueil">
+
                         
                         </div>
-                       
+                        <p>Il suffit de modifier les informations contenues dans le formulaire qui s'affiche et de cliquer sur valider.</p>
+                        <img src="{{ asset('image/cat-form.PNG') }}" alt="accueil" class="img-fluid accueil">
+                        <img src="{{ asset('image/site-modifier-cat.png') }}" alt="accueil" class="  img-fluid accueil">
                   </div>
   
                       
