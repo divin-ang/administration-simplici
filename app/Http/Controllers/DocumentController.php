382d17380a36fefa1f3553a5bfb0b038bb03d280
div-ang <angapaydivin@gmail.com>
2021-08-05 08:05:02 +0100

    ajout d'images tutoriel

--- app/Http/Controllers/DocumentController.php
+++ app/Http/Controllers/DocumentController.php
@@ -105,7 +105,7 @@ class DocumentController extends Controller
          ->where('process_annex_documents.process_id',$processId)
          ->join('annex_documents','process_annex_documents.annex_document_id','=','annex_documents.annex_document_id')
          ->get();
-         return view('demarches/modifierDocument',[ 'annex_document'=>$documents, 'process'=>$process[0]]);
+         return view('demarches/afficherDocuments',[ 'documents'=>$documents, 'process'=>$process[0]]);
      }
   public function supprimerDocument($id,$processId){
     DB::table('process_annex_documents')
@@ -120,7 +120,7 @@ class DocumentController extends Controller
     ->join('annex_documents','process_annex_documents.annex_document_id','=','annex_documents.annex_document_id')
     ->select('annex_documents.*')
     ->get();
-    return view('demarches/modifierDocument',[ 'annex_document'=>$documents, 'process'=>$process[0]]);
+    return view('demarches/afficherDocuments',[ 'documents'=>$documents, 'process'=>$process[0]]);
 
   }
 }
