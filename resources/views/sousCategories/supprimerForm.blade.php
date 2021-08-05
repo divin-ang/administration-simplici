0c97ba3cb67c56f2d67da037c9cb1fd6a56681af
div-ang <angapaydivin@gmail.com>
2021-08-05 11:55:31 +0100

    ajout d'images tutoriel

--- resources/views/sousCategories/supprimerForm.blade.php
+++ resources/views/sousCategories/supprimerForm.blade.php
@@ -6,7 +6,7 @@
     <div class="row justify-content-center">
         <div class="col-md-10">
             <div class="card">
-                <div class="card-header">{{ __('') }} {{ $sous_categorie->subcontainer__name}}</div>
+                <div class="card-header">{{ __('') }} {{ $sous_categorie->subcontainer_name}}</div>
 
                 <div class="card-body">
                     @if (session('status'))
